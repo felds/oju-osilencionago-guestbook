2a1ec434686441d3b1d24d2d27d610f1bde3958b
Felds Liscia <dev@felds.com.br>
2023-11-14 17:47:59 -0300

    add app icon to satisfy pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import basicSsl from "@vitejs/plugin-basic-ssl"
 import { VitePWA } from "vite-plugin-pwa"
 
+const pwaPlugin = VitePWA({
+    registerType: "autoUpdate",
+    devOptions: { enabled: true },
+    manifest: {
+        background_color: "#742134",
+        icons: [{ src: "appicon-144-144.png", type: "image/png", sizes: "144x144" }],
+    },
+})
+
 export default {
-    plugins: [basicSsl(), VitePWA({ registerType: "autoUpdate" })],
+    plugins: [basicSsl(), pwaPlugin],
 }
